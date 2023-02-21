2fbec8d2eda02fa69fc53d5997e92e362c86d5ae
LegandaryPaul <paulanjelozzz@gmail.com>
2023-02-21 10:48:08 +0800

    update

---
+++ dist/script.js
@@ -0,0 +1,7 @@
+// burger button function
+const toggleMenu = document.querySelector(".toggle__menu");
+const headerNav = document.querySelector(".header__nav");
+toggleMenu.addEventListener("click", () => {
+  toggleMenu.classList.toggle("open");
+  headerNav.classList.toggle("open");
+});
